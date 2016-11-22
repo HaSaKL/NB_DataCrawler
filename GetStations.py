bb39bb44bb3855e038b531aabb1d49fc73083c05
HaSaKL <salewski@wiwi.uni-kl.de>
2016-11-22 16:41:15 +0100

    Station-Info URL is read from Database "login.db" + Refactoring

--- GetStations.py
+++ GetStations.py
@@ -1,21 +1,42 @@
 import urllib.request
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as ElmTree
+import sqlite3
 
-getStationsURL = "https://nextbike.net/maps/nextbike-official.xml"
 
-if __name__ == '__main__':
-    print("opening URL")
-    response = urllib.request.urlopen(getStationsURL)
-    xml_data = response.read()
-    xml_data = xml_data.decode()
-    root = ET.fromstring(xml_data)
+def get_url_from_db():
+    """Returns the URL to receive general station info and current station occupation from the Database"""
+    try:
+        conn = sqlite3.connect("login.db")
+        c = conn.cursor()
+        c.execute("SELECT urls.url FROM urls WHERE urls.name = 'StationList'")
+        url = c.fetchone()[0]
+        conn.close()
+        return url
+    except sqlite3.Error:
+        print("Error getting URL from Database")
 
+
+def get_stations_status():
+    """Returns an XML-object with the current status of all stations world-wide"""
+    url = get_url_from_db()
+    response = urllib.request.urlopen(url)
+    xml_tree = response.read().decode()
+    return xml_tree
+
+
+def print_xml_data(data):
+    """Prints the list of stations from the current-status-xml-file on screen"""
+    root = ElmTree.fromstring(data)
     print(root)
-
     for country in root:
         print(country.attrib.get("country_name"), ": ", country.attrib.get("name"))
         for city in country:
-            print("\n", city.attrib.get("name" ), "; ",  city.tag, "-uid:", city.attrib.get("uid"))
+            print("\n", city.attrib.get("name"), "; ", city.tag, "-uid:", city.attrib.get("uid"))
             for place in city:
                 print(place.attrib.get("name"), "; ", place.tag, "-uid:", place.attrib.get("uid"))
         print("----------------------------------------------------")
+
+
+if __name__ == '__main__':
+    xml_data = get_stations_status()
+    print_xml_data(xml_data)
