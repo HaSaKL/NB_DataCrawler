d4d791e0246cf3466782c419d5d1f8b8a0b01d61
HaSaKL <salewski@wiwi.uni-kl.de>
2016-11-25 16:40:33 +0100

    Method for updateing master data; Added table for country data

--- GetStations.py
+++ GetStations.py
@@ -1,6 +1,7 @@
 import urllib.request
 import xml.etree.ElementTree as ElmTree
 import sqlite3
+import datetime
 
 
 def get_url_from_db():
@@ -22,11 +23,18 @@ def get_url_from_db():
 
 
 def get_stations_status():
-    """Returns an XML-object with the current status of all stations world-wide"""
+    """Returns an XML-object with the current status of all stations world-wide and a datetime of query as tuple"""
     url = get_url_from_db()
     response = urllib.request.urlopen(url)
     xml_tree = response.read().decode()
-    return xml_tree
+
+    # get the time of the query from the comment at the end of the file
+    query_time_start_pos = xml_tree.find("<!--")
+    query_time_end_pos = xml_tree.find("-->")
+    time_string = xml_tree[query_time_start_pos+5 : query_time_end_pos-1]
+    time = datetime.datetime.strptime(time_string, "%d.%m.%Y %H:%M")
+
+    return (xml_tree, time)
 
 
 def print_xml_data(data):
@@ -54,12 +62,82 @@ def connect_stations_db():
     except sqlite3.OperationalError:
         c.execute("CREATE TABLE `places_data` (`uid` INTEGER NOT NULL,`number` INTEGER, `spot` INTEGER, "
                   "`name` TEXT, `latitude` REAL, `longitude` REAL,`terminal_type` TEXT, PRIMARY KEY(`uid`))")
-        c.execute("CREATE TABLE `city_data` ( `uid` INTEGER NOT NULL, `name` TEXT, `country` TEXT, "
-                  "`num_places` INTEGER, `latitude` REAL, `longitude` REAL, PRIMARY KEY(`uid`) )")
+        c.execute("CREATE TABLE `city_data` ( `uid` INTEGER NOT NULL, `name` TEXT,`num_places` INTEGER, "
+                  "`latitude` REAL, `longitude` REAL, PRIMARY KEY(`uid`) )")
+        c.execute("CREATE TABLE `country_data` ( `domain` TEXT NOT NULL, `name` TEXT NOT NULL, "
+                  "`country` TEXT NOT NULL, `latitute` REAL, `longitude` REAL, PRIMARY KEY(`domain`) )")
     finally:
         return conn
 
 
+def update_city_info_from_xml(data, conn):
+    """"Writes general city data from a provided xml-file to the database"""
+    c = conn.cursor()
+    root = ElmTree.fromstring(data)
+    for country in root:
+        for city in country:
+            uid = city.attrib.get("uid")
+            name = city.attrib.get("name")
+            lat = city.attrib.get("lat")
+            lng = city.attrib.get("lng")
+            num_places = city.attrib.get("num_places")
+            c.execute("INSERT OR IGNORE INTO city_data VALUES (?, ?, ?, ?, ?)", (uid, name, num_places, lat, lng))
+    conn.commit()
+
+
+def update_station_info_from_xml(data, conn):
+    """Writes general stations data from a provided xml-file to the database """
+    c = conn.cursor()
+    root = ElmTree.fromstring(data)
+    for country in root:
+        for city in country:
+            for place in city:
+                uid = place.attrib.get("uid")
+                number = place.attrib.get("number")
+                name = place.attrib.get("name")
+                spot = place.attrib.get("spot")
+                lat = place.attrib.get("lat")
+                lng = place.attrib.get("lng")
+                terminal_type = place.attrib.get("terminal_type")
+                c.execute("INSERT OR IGNORE INTO places_data VALUES (?, ?, ?, ?, ?, ?, ?)",
+                          (uid, number, spot, name, lat, lng, terminal_type))
+    conn.commit()
+
+
+def update_country_info_from_xml(data, conn):
+    """"Writes general country data from a provided xml-file to the database """
+    c = conn.cursor()
+    root = ElmTree.fromstring(data)
+    for country_item in root:
+        domain = country_item.attrib.get("domain")
+        name = country_item.attrib.get("name")
+        country = country_item.attrib.get("country")
+        lat = country_item.attrib.get("lat")
+        lng = country_item.attrib.get("lng")
+        c.execute("INSERT OR IGNORE INTO country_data VALUES (?, ?, ?, ?, ?)",
+                  (domain, name, country, lat, lng))
+    conn.commit()
+
+
+def write_master_data_from_web_to_db():
+    """Reads the general stations data from the web and writes it to the database"""
+    print("Opening web connection and downloading stations data")
+    xml_data, time = get_stations_status()
+    print("done.")
+
+    print("Time of query: ", time)
+
+    print("Writing City Data to Database...")
+    conn = connect_stations_db()
+    update_city_info_from_xml(xml_data, conn)
+    print("Writing Stations Data to Database...")
+    update_station_info_from_xml(xml_data, conn)
+    print("Writing Country Data to Database...")
+    update_country_info_from_xml(xml_data, conn)
+    print("done.")
+
+
 if __name__ == '__main__':
-    xml_data = get_stations_status()
+    xml_data, time = get_stations_status()
     print_xml_data(xml_data)
+    print("Time of query: ",time)
